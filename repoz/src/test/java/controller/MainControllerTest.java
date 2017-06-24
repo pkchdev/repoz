f78a7c7f01912ecc79e3bc936e4b961ab926073e
pkchdev <chtitpierre78@hotmail.com>
2017-06-24 19:36:35 +0200

    Add test page not found

--- repoz/src/test/java/controller/MainControllerTest.java
+++ repoz/src/test/java/controller/MainControllerTest.java
@@ -3,6 +3,8 @@ package controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.UUID;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import ch.qos.logback.classic.pattern.Util;
 import repoz.Application;
 
 
@@ -28,6 +31,12 @@ public class MainControllerTest {
     public void getIndex() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+    
+    @Test
+    public void getNotFound() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 	
 }
